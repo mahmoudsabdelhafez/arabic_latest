50661b5eb2577c275a85498de1dbd0d4bf1f80aa
mahmoudsabdelhafez <mah.abdelhafez@gmail.com>
2025-02-05 11:51:41 +0300

    Update 2025_02_01_170005_create_arabic_tool_letter_table.php

--- Arabic/database/migrations/2025_02_01_170005_create_arabic_tool_letter_table.php
+++ Arabic/database/migrations/2025_02_01_170005_create_arabic_tool_letter_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArabicToolLetterTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
         Schema::create('arabic_tool_letter', function (Blueprint $table) {
             $table->id();
             $table->foreignId('arabic_tool_id')->constrained()->onDelete('cascade');
-            // $table->foreignId('arabic_letter_id')->constrained()->onDelete('cascade');
                     $table->foreignId('arabic_letter_id')->constrained('arabic_letters')->onDelete('cascade');
 
             $table->string('effect')->nullable();
